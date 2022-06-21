eab19740b2a4c8bb77d534da5da068dd9b5efc00
Caden Haustein <code@brightlysalty.33mail.com>
2022-06-21 15:53:55 -0500

    Move to docs instead of site as the destination directory

--- site.hs
+++ site.hs
@@ -6,7 +6,7 @@ import           Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith (defaultConfiguration {destinationDirectory = "docs"}) $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
